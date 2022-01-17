ed1c624e10c52fdb1c9eb2b9258cbf75b04a9fb9
cristivasile <79210106+cristivasile@users.noreply.github.com>
2022-01-17 12:53:51 +0200

    Create frontend project

--- Frontend/Angular/src/app/app-routing.module.ts
+++ Frontend/Angular/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'auth',
+    loadChildren : () => import('src/app/modules/auth/auth.module').then(x => x.AuthModule)
+  },
+  {
+    path: '',
+    loadChildren : () => import('src/app/modules/vehicles/vehicles.module').then(x => x.VehiclesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Frontend/Angular/src/app/modules/vehicles/vehicles-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { VehicleComponent } from './vehicle/vehicle.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'vehicles'
+  },
+  {
+    path:'vehicles',
+    component: VehicleComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class VehiclesRoutingModule { }
